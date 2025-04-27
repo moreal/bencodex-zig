707dea1e785d993be195ffe0ee00e698bded4072
Lee Dogeon <dev.moreal@gmail.com>
2025-04-28 00:46:01 +0900

    refactor: export modules directly

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 
-pub const bencodex = struct {
-    pub const types = @import("bencodex/types.zig");
-    pub const encode = @import("bencodex/encode.zig");
-    pub const decode = @import("bencodex/decode.zig");
-    pub const errors = @import("bencodex/errors.zig");
-};
-
-pub usingnamespace bencodex;
+pub const types = @import("bencodex/types.zig");
+pub const encode = @import("bencodex/encode.zig");
+pub const decode = @import("bencodex/decode.zig");
+pub const errors = @import("bencodex/errors.zig");
